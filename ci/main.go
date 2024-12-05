653736e81c18510a4a02d8611d319d96d58b753e
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2024-12-05 16:51:59 +0100

    feat: Initial version of new module

---
+++ ci/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	// "context"
+	"dagger/ci/internal/dagger"
+    // "strings"
+
+    // "gopkg.in/yaml.v3"
+)
+
+type Ci struct{}
+
+// func ReadTaskFile(ctx context.Context, taskFile *dagger.File) CiData {
+//     yamlContent, err := taskFile.Contents(ctx)
+//     if err != nil {
+//         panic(err)
+//     } 
+
+//     ciData := CiData{}
+
+//     err = yaml.Unmarshal([]byte(yamlContent), &ciData)
+//     if err != nil {
+//         panic(err)
+//     } 
+
+//     return ciData
+// }
+
+func (m *Ci) ExecuteTask(task CiTask, container *dagger.Container) *dagger.Container {
+    if task.Run == "" {
+    //     container.WithExec(strings.Split(task.Run, " "))
+    }
+
+    return container
+}
+
+// func (m *Ci) ExecuteTask(ctx context.Context, task CiTask, container dagger.Container) *dagger.Container {
+//     if task.Run {
+//         container.WithExec(strings.Split(task.Run, " "))
+//     }
+
+//     return container
+// }
+
+// func (m *Ci) ExecuteCI(ctx context.Context, taskFile *dagger.File) *dagger.Container {
+//     ciData := ReadTaskFile(ctx, taskFile)
+
+//     dockerImage := ciData.Setup.Technology + ":" + ciData.Setup.Version
+
+//     container, err := dag.Container().From(dockerImage)
+
+//     for _, task := range ciData.Defaults.Tasks {
+//         container = m.ExecuteTask(ctx, task, container)
+//     }
+
+//     return container
+// }
+
+func (m *Ci) ContainerEcho(stringArg string) *dagger.Container {
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+}
+
---
+++ ci/types.go
@@ -0,0 +1,26 @@
+package main
+
+type CiTask struct {
+    Name       string            `yaml:"name"`
+    Needs      []string          `yaml:"needs"`
+    WorkingDir string            `yaml:"working_dir"`
+    Run        string            `yaml:"run"`
+    Uses       string            `yaml:"uses"`
+    Function   string            `yaml:"function"`
+    With       map[string]string `yaml:"with"`
+    Image      string            `yaml:"image"`
+}
+
+type CiTaskList struct {
+    Tasks      []CiTask          `yaml:"tasks"`
+}
+
+type CiSetup struct {
+    Technology string            `yaml:"technology"`
+    Version    string            `yaml:"version"`
+}
+
+type CiData struct {
+    Setup      CiSetup           `yaml:"setup"`
+    Defaults   CiTaskList        `yaml:"defaults"`
+}
