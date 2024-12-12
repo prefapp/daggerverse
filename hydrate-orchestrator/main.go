3f846958ad9d026614af6ca214f72c71ffe015dd
Jose Alvarez <jose.alvarez@prefapp.es>
2024-12-12 19:36:53 +0100

    Add gh logic

--- hydrate-orchestrator/main.go
+++ hydrate-orchestrator/main.go
@@ -4,19 +4,29 @@ import (
 	"context"
 	"dagger/hydrate-orchestrator/internal/dagger"
 	"encoding/json"
+	"fmt"
 	"path/filepath"
+	"strings"
 )
 
 type HydrateOrchestrator struct {
 	WetRepoDir *dagger.Directory
+	ghToken    dagger.Secret
 }
 
 func New(
+
+	// Github token
+	// +required
+	ghToken dagger.Secret,
 	// The path to the wet repo directory, where the wet manifests are stored
 	// +optional
 	wetRepoDir *dagger.Directory,
 ) *HydrateOrchestrator {
-	return &HydrateOrchestrator{}
+	return &HydrateOrchestrator{
+		WetRepoDir: wetRepoDir,
+		ghToken:    ghToken,
+	}
 }
 
 func (m *HydrateOrchestrator) Run(
@@ -40,9 +50,6 @@ func (m *HydrateOrchestrator) Run(
 	var deployments []string
 	json.Unmarshal([]byte(updatedDeployments), &deployments)
 
-	// Iterate over the deployments
-	kubernetesDeployments := make([]string, 0)
-
 	for _, deployment := range deployments {
 		// Get the first directory from the deployment string
 		deploymentType := filepath.SplitList(deployment)[1]
@@ -62,7 +69,45 @@ func (m *HydrateOrchestrator) Run(
 				AffectedPaths:   string(affectedJson),
 				NewImagesMatrix: newImagesMatrix,
 			})
-			kubernetesDeployments = append(kubernetesDeployments, deployment)
+
+			// Create a branch for the updated deployment
+			wetRepoPath := "/wet_repo"
+			// split the path into a slice
+			deploymentPath := strings.Split(deployment, "/")
+			depType := deploymentPath[0]
+			cluster := deploymentPath[1]
+			tenant := deploymentPath[2]
+			env := deploymentPath[3]
+
+			branchName := fmt.Sprintf("%s-%s-%s-%s", depType, cluster, tenant, env)
+
+			dag.Gh().Container(dagger.GhContainerOpts{
+				Token: &m.ghToken,
+			}).
+				WithDirectory(wetRepoPath, m.WetRepoDir).
+				WithWorkdir(wetRepoPath).
+				WithExec([]string{
+					"git",
+					"checkout",
+					"-b",
+					branchName,
+				}, dagger.ContainerWithExecOpts{},
+				).WithExec([]string{
+				"git",
+				"push",
+				"origin",
+				branchName,
+			})
+
+			// Create each label
+			dag.Gh(dagger.GhOpts{Token: &m.ghToken}).Run(fmt.Sprintf("label create --force type/%s", depType))
+			dag.Gh(dagger.GhOpts{Token: &m.ghToken}).Run(fmt.Sprintf("label create --force app/%s", app))
+			dag.Gh(dagger.GhOpts{Token: &m.ghToken}).Run(fmt.Sprintf("label create --force cluster/%s", cluster))
+			dag.Gh(dagger.GhOpts{Token: &m.ghToken}).Run(fmt.Sprintf("label create --force tenant/%s", tenant))
+			dag.Gh(dagger.GhOpts{Token: &m.ghToken}).Run(fmt.Sprintf("label create --force env/%s", env))
+
+			// Create a PR for the updated deployment
+			dag.Gh(dagger.GhOpts{Token: &m.ghToken}).Run(fmt.Sprintf("pr create --title 'Update deployment' --body 'Update deployment' --base main --head %s", branchName))
 
 		}
 	}
