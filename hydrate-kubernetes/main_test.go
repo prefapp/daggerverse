b44498c261ac5b61fa53e2118d5f4e8252becd63
Toni R. Vega <91343444+tonirvega@users.noreply.github.com>
2024-12-16 12:04:37 +0100

    fix: execs check go template (#50)
    * fix: execs go template
    
    * add testing for render without execs key

--- hydrate-kubernetes/main_test.go
+++ hydrate-kubernetes/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"gopkg.in/yaml.v3"
@@ -71,6 +72,76 @@ func TestRenderAppsCanRenderNewImages(t *testing.T) {
 	if artifact.Metadata.Annotations.Image != "new-image:1.0.0" {
 
 		t.Errorf("Expected new Deployment.sample-app-micro-a.yml to have image new-image:1.0.0, got %s", artifact.Metadata.Annotations)
+
+	}
+}
+
+func TestRenderAppsCanRenderNewImagesWithoutExecs(t *testing.T) {
+
+	ctx := context.Background()
+
+	valuesRepoDir := getDir("./fixtures/values-repo-dir")
+
+	wetRepoDir := getDir("./fixtures/wet-repo-dir")
+
+	helmDir := getDir("./helm")
+
+	m := &HydrateKubernetes{
+		ValuesDir:    valuesRepoDir.Directory("fixtures/values-repo-dir"),
+		WetRepoDir:   wetRepoDir.Directory("fixtures/wet-repo-dir"),
+		Container:    dag.Container().From("ghcr.io/helmfile/helmfile:latest"),
+		Helmfile:     helmDir.File("helm/helmfile.yaml"),
+		ValuesGoTmpl: helmDir.File("helm/values.yaml.gotmpl"),
+	}
+
+	depsContent, errContents := valuesRepoDir.
+		File("./fixtures/values-repo-dir/.github/hydrate_deps.yaml").
+		Contents(ctx)
+
+	if errContents != nil {
+
+		t.Errorf("Error reading deps file: %v", errContents)
+
+	}
+
+	m.Container = installDeps(depsContent, m.Container)
+
+	renderedDir := m.RenderApps(
+		ctx,
+		"[\"kubernetes/cluster-name/test-tenant/without_execs/values.yaml\"]",
+		"{\"images\":[]}",
+		"sample-app",
+	)
+
+	fmt.Printf("Rendered dir: %v", renderedDir)
+
+	newDpRendered := renderedDir.File("kubernetes/cluster-name/test-tenant/dev/Deployment.sample-app-micro-a.yml")
+
+	if newDpRendered == nil {
+		t.Errorf("Expected new Deployment.sample-app-micro-a.yml to be rendered")
+	}
+
+	artifact := Artifact{}
+
+	content, err := newDpRendered.Contents(ctx)
+
+	if err != nil {
+
+		t.Errorf("Error reading new Deployment.sample-app-micro-a.yml: %v", err)
+
+	}
+
+	errUnms := yaml.Unmarshal([]byte(content), &artifact)
+
+	if errUnms != nil {
+
+		t.Errorf("Error unmarshalling new Deployment.sample-app-micro-a.yml: %v", errUnms)
+
+	}
+
+	if artifact.Metadata.Annotations.Image != "image-a:1.16.0" {
+
+		t.Errorf("Expected new Deployment.sample-app-micro-a.yml to have image image-a:1.16.0, got %s", artifact.Metadata.Annotations)
 
 	}
 }
