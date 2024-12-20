6937821f13fae17de7ab28162e2e3162682328fe
Jose Alvarez <47305925+jalvarezit@users.noreply.github.com>
2024-12-20 13:01:09 +0100

    fix: Ignore environments and repositories files (#72)
    * Ignore environments and repositories files
    
    * Add unit testing
    
    ---------
    
    Co-authored-by: Jose Alvarez <jose.alvarez@prefapp.es>
    Co-authored-by: Toni R. Vega <91343444+tonirvega@users.noreply.github.com>

--- hydrate-orchestrator/test_launch_helm_auth.sh
+++
@@ -1,1 +0,0 @@
-dagger call --repo "firestartr-test/state-app-sample-app" --gh-token=cmd:'gh auth token' --app "sample-app" --values-state-dir "/home/arvega/projects/firestartr-test/state-app-sample-app-main" --wet-state-dir "/home/arvega/projects/firestartr-test/state-app-sample-app" get-helm-auth --auth-dir "/tmp/test" $1
