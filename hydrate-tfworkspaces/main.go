82aa38d5399a3494518a3b70ce4fa0713390a366
tonirvega <arvegadev@hotmail.com>
2025-02-07 09:26:13 +0100

    wip

--- hydrate-tfworkspaces/main.go
+++ hydrate-tfworkspaces/main.go
@@ -15,23 +15,130 @@
 package main
 
 import (
-	"context"
 	"dagger/hydrate-tfworkspaces/internal/dagger"
+	"fmt"
+	"path"
 )
 
-type HydrateTfworkspaces struct{}
-
-// Returns a container that echoes whatever string argument is provided
-func (m *HydrateTfworkspaces) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+type HydrateTfworkspaces struct {
+	FirestarterImage            string
+	FirestarterImageTag         string
+	GithubAppID                 string
+	GithubInstallationID        string
+	GithubPrefappInstallationID string
+	GithubPrivateKey            *dagger.Secret
+	GithubOrganization          string
+	GhToken                     *dagger.Secret
+	ClaimsDefaultBranch         string // +default="main"
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *HydrateTfworkspaces) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
+func New(
+	// +optional
+	// +default="latest-slim"
+	firestarterImageTag string,
+	// +optional
+	// +default="ghcr.io/prefapp/gitops-k8s"
+	firestarterImage string,
+	// +required
+	// Github application ID
+	githubAppID string,
+	// +required
+	// Github installation ID
+	githubInstallationID string,
+	// +required
+	// Github prefapp installation ID
+	githubPrefappInstallationID string,
+	// +required
+	// Github private key
+	githubPrivateKey *dagger.Secret,
+	// +required
+	// Github organization
+	githubOrganization string,
+	// +required
+	// Github token
+	ghToken *dagger.Secret,
+	// +default="main"
+	claimsDefaultBranch string,
+
+) *HydrateTfworkspaces {
+	return &HydrateTfworkspaces{
+		FirestarterImage: firestarterImage,
+
+		FirestarterImageTag: firestarterImageTag,
+
+		GithubAppID: githubAppID,
+
+		GithubInstallationID: githubInstallationID,
+
+		GithubPrefappInstallationID: githubPrefappInstallationID,
+
+		GithubPrivateKey: githubPrivateKey,
+
+		GithubOrganization: githubOrganization,
+
+		GhToken: ghToken,
+
+		ClaimsDefaultBranch: claimsDefaultBranch,
+	}
+}
+
+func (m *HydrateTfworkspaces) CmdHydrate(
+	// Claims repository name
+	// +required
+	claimsRepo string,
+	// Claims directory
+	// +required
+	claimsDir *dagger.Directory,
+	// Previous CRs directory
+	// +required
+	crsDir *dagger.Directory,
+	// Provider to render
+	// +required
+	provider string,
+	// GitHub application ID
+	// +required
+) *dagger.Directory {
+
+	fmt.Printf(fmt.Sprintf("Hydrating CRs for %s", provider))
+
+	claimsTargetDir := "/claims"
+	crsTargetDir := "/crs"
+	outputDir := "/output"
+
+	cmd := m.CmdContainer().
+		WithMountedDirectory(claimsTargetDir, claimsDir).
+		WithMountedDirectory(crsTargetDir, crsDir).
+		WithEnvVariable("GITHUB_APP_ID", m.GithubAppID).
+		WithEnvVariable("GITHUB_INSTALLATION_ID", m.GithubInstallationID).
+		WithEnvVariable("GITHUB_APP_INSTALLATION_ID_PREFAPP", m.GithubPrefappInstallationID).
+		WithSecretVariable("GITHUB_APP_PEM_FILE", m.GithubPrivateKey).
+		WithEnvVariable("ORG", m.GithubOrganization).
+		WithEnvVariable("DEBUG", "firestartr-test:*").
+		WithExec(
+			[]string{
+				"./run.sh",
+				"cdk8s",
+				"--render",
+				"--disableRenames",
+				"--globals", path.Join(crsTargetDir, ".config"),
+				"--initializers", path.Join(crsTargetDir, ".config"),
+				"--claims", path.Join(claimsTargetDir, "claims"),
+				"--previousCRs", crsTargetDir,
+				"--excludePath", path.Join(crsTargetDir, ".github"),
+				"--claimsDefaults", path.Join(claimsTargetDir, ".config"),
+				"--outputCrDir", outputDir,
+				"--validateReferentialIntegrity", "disabled",
+				"--provider", provider,
+			},
+		)
+
+	return cmd.Directory(outputDir)
+
+}
+
+func (m *HydrateTfworkspaces) CmdContainer() *dagger.Container {
+
 	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
+		From(fmt.Sprintf("%s:%s", m.FirestarterImage, m.FirestarterImageTag))
+
 }
