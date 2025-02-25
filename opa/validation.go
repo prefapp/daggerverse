52acbf4325613b60a1ca41f73c90d20a944f9185
tonirvega <arvegadev@hotmail.com>
2025-02-25 17:27:46 +0100

    fix: move functions to utils

--- opa/validation.go
+++ opa/validation.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"dagger/opa/internal/dagger"
 	"fmt"
-	"regexp"
 	"time"
 )
 
@@ -78,23 +77,4 @@ func (m *Opa) Validate(
 	}
 
 	return ctr, nil
-}
-
-func strip(str string) string {
-
-	const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
-
-	var re = regexp.MustCompile(ansi)
-
-	str = re.ReplaceAllString(str, "")
-
-	str = regexp.MustCompile(`\n`).ReplaceAllString(str, "")
-
-	// remove carriage return
-	str = regexp.MustCompile(`\r`).ReplaceAllString(str, "")
-
-	// remove line feed
-	str = regexp.MustCompile(`\f`).ReplaceAllString(str, "")
-
-	return str
 }
