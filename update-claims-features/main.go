c8f34bfe51974da117aafc222af62fa58b5bc7ea
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2025-04-02 10:26:29 +0200

    fix: Wrong token used for features releases

--- update-claims-features/main.go
+++ update-claims-features/main.go
@@ -125,7 +125,7 @@ func (m *UpdateClaimsFeatures) UpdateAllClaimFeatures(
 	ghReleaseListResult, err := dag.Gh(dagger.GhOpts{
 		Version: m.GhCliVersion,
 	}).Container(dagger.GhContainerOpts{
-		Token: m.GhToken,
+		Token: m.PrefappGhToken,
 		Repo:  "prefapp/features",
 	}).WithDirectory(m.ClaimsDirPath, m.ClaimsDir, dagger.ContainerWithDirectoryOpts{}).
 		WithWorkdir(m.ClaimsDirPath).
