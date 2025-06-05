e1e4e3095037703716dca38ead39dbb730e86c8e
Toni R. Vega <arvegadev@hotmail.com>
2025-06-05 17:21:29 +0200

    Update main.go

--- hydrate-orchestrator/modules/hydrate-tfworkspaces/main.go
+++ hydrate-orchestrator/modules/hydrate-tfworkspaces/main.go
@@ -10,7 +10,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var FIRESTARTR_DOCKER_IMAGE = "ghcr.io/prefapp/gitops-k8s:v1.39.2_slim"
+var FIRESTARTR_DOCKER_IMAGE = "ghcr.io/prefapp/gitops-k8s:v1.41.1_slim"
 
 type HydrateTfworkspaces struct {
 	ValuesDir        *dagger.Directory
