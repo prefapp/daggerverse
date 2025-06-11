457cffe03e66ac9dee156f06df547d9c75a2ef48
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2025-06-11 13:35:57 +0200

    fix: Missing fields in update-claims-features module

--- update-claims-features/types.go
+++ update-claims-features/types.go
@@ -33,36 +33,73 @@ type ReleaseBody struct {
 }
 
 type Claim struct {
-	Kind        string            `yaml:"kind,omitempty"`
-	Version     string            `yaml:"version,omitempty"`
-	Type        string            `yaml:"type,omitempty"`
-	Lifecycle   string            `yaml:"lifecycle,omitempty"`
-	System      string            `yaml:"system,omitempty"`
-	Name        string            `yaml:"name,omitempty"`
-	Annotations map[string]string `yaml:"annotations,omitempty"`
-	Providers   Providers         `yaml:"providers,omitempty"`
-	Owner       string            `yaml:"owner,omitempty"`
+	Kind          string            `yaml:"kind,omitempty"`
+	Version       string            `yaml:"version,omitempty"`
+	Type          string            `yaml:"type,omitempty"`
+	Lifecycle     string            `yaml:"lifecycle,omitempty"`
+	System        string            `yaml:"system,omitempty"`
+	Name          string            `yaml:"name,omitempty"`
+	Description   string            `yaml:"description,omitempty"`
+	Owner         string            `yaml:"owner,omitempty"`
+	Annotations   map[string]string `yaml:"annotations,omitempty"`
+	Providers     Providers         `yaml:"providers,omitempty"`
+	PlatformOwner string            `yaml:"platformOwner,omitempty"`
+	MaintainedBy  []string          `yaml:"maintainedBy,omitempty"`
+	Profile       Profile           `yaml:"profile,omitempty"`
 }
 
 type Providers struct {
 	Github Github `yaml:"github,omitempty"`
 }
 
+type Profile struct {
+	DisplayName string `yaml:"displayName,omitempty"`
+	Email       string `yaml:"email,omitempty"`
+	Picture     string `yaml:"picture,omitempty"`
+}
+
 type Github struct {
-	Description        string         `yaml:"description,omitempty"`
-	Name               string         `yaml:"name,omitempty"`
-	Org                string         `yaml:"org,omitempty"`
-	Visibility         string         `yaml:"visibility,omitempty"`
-	AdditionalBranches []Branch       `yaml:"additionalBranches,omitempty"`
-	BranchStrategy     BranchStrategy `yaml:"branchStrategy,omitempty"`
-	Actions            Actions        `yaml:"actions,omitempty"`
-	Overrides          Overrides      `yaml:"overrides,omitempty"`
-	Features           []Feature      `yaml:"features,omitempty"`
+	Description         string         `yaml:"description,omitempty"`
+	Name                string         `yaml:"name,omitempty"`
+	Org                 string         `yaml:"org,omitempty"`
+	Visibility          string         `yaml:"visibility,omitempty"`
+	DefaultBranch       string         `yaml:"defaultBranch,omitempty"`
+	OrgPermissions      string         `yaml:"orgPermissions,omitempty"`
+	Technology          Technology     `yaml:"technology,omitempty"`
+	AdditionalBranches  []Branch       `yaml:"additionalBranches,omitempty"`
+	BranchStrategy      BranchStrategy `yaml:"branchStrategy,omitempty"`
+	AllowSquashMerge    bool           `yaml:"allowSquashMerge,omitempty"`
+	AllowMergeCommit    bool           `yaml:"allowMergeCommit,omitempty"`
+	AllowRebaseMerge    bool           `yaml:"allowRebaseMerge,omitempty"`
+	DeleteBranchOnMerge bool           `yaml:"deleteBranchOnMerge,omitempty"`
+	AutoInit            bool           `yaml:"autoInit,omitempty"`
+	ArchiveOnDestroy    bool           `yaml:"archiveOnDestroy,omitempty"`
+	AllowUpdateBranch   bool           `yaml:"allowUpdateBranch,omitempty"`
+	HasIssues           bool           `yaml:"hasIssues,omitempty"`
+	Pages               Pages          `yaml:"pages,omitempty"`
+	Actions             Actions        `yaml:"actions,omitempty"`
+	Overrides           Overrides      `yaml:"overrides,omitempty"`
+	Features            []Feature      `yaml:"features,omitempty"`
+}
+
+type Technology struct {
+	Stack   string `yaml:"stack,omitempty"`
+	Version string `yaml:"version,omitempty"`
 }
 
 type BranchStrategy struct {
 	Name          string `yaml:"name,omitempty"`
 	DefaultBranch string `yaml:"defaultBranch,omitempty"`
+}
+
+type Pages struct {
+	Cname  string `yaml:"cname,omitempty"`
+	Source Source `yaml:"source,omitempty"`
+}
+
+type Source struct {
+	Branch string `yaml:"branch,omitempty"`
+	Path   string `yaml:"path,omitempty"`
 }
 
 type Actions struct {
