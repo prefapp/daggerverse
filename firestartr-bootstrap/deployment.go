f7cb37d2c923a6745f15a36dde18ab6a6c5f1174
Jose Alvarez <jose.alvarez@prefapp.es>
2026-02-19 11:40:36 +0100

    fix: Update GitHub App paths in deployment rendering

--- firestartr-bootstrap/deployment.go
+++ firestartr-bootstrap/deployment.go
@@ -96,14 +96,14 @@ func (m *FirestartrBootstrap) RenderDeployment(
 
 				GithubAppId: fmt.Sprintf(
 
-					"/firestartr/%s/fs-%s-admin/app-id",
+					"/firestartr/%s/fs-%s/app-id",
 
 					m.Bootstrap.Customer,
 					m.Bootstrap.Customer,
 				),
 				GithubAppInstallationId: fmt.Sprintf(
 
-					"/firestartr/%s/fs-%s-admin/%s/app-installation-id",
+					"/firestartr/%s/fs-%s/%s/app-installation-id",
 
 					m.Bootstrap.Customer,
 					m.Bootstrap.Customer,
@@ -111,7 +111,7 @@ func (m *FirestartrBootstrap) RenderDeployment(
 				),
 				GithubAppPem: fmt.Sprintf(
 
-					"/firestartr/%s/fs-%s-admin/pem",
+					"/firestartr/%s/fs-%s/pem",
 
 					m.Bootstrap.Customer,
 					m.Bootstrap.Customer,
